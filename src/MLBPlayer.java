89bb135bdbc908805c86ef9e9033ca4fafa838b1
RamziJabali <ramzieljabali@gmail.com>
2019-12-04 15:52:01 -0700

    put them in files

---
+++ src/MLBPlayer.java
@@ -0,0 +1,23 @@
+public class MLBPlayer extends MLBPlayerKey {
+    private String playerName;
+    private double battingAverage;
+
+    public MLBPlayer(String teamName_, String playerName_, int battingAverage_, int jerseyNum_) {
+        super(jerseyNum_, teamName_);
+        playerName = playerName_;
+        battingAverage = battingAverage_;
+    }
+
+    public double getBattingAverage() {
+        return battingAverage;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+    @Override
+    public String toString() {
+        return "Player " + playerName + "Has a batting average of" + battingAverage + "";
+    }
+}
+
---
+++ src/Table.java
@@ -0,0 +1,66 @@
+public class Table {
+    private TNode root;
+
+    public Table(Keyed key) {
+        root = new TNode(key);
+    }
+
+    public Table() {
+        root = null;
+    }
+
+    public void addNode(Keyed value) {
+        TNode newNode = new TNode(value);
+        if (root == null) {
+            root = newNode;
+        } else {
+            TNode focusNode = root;//have to start with root as we traverse
+            TNode parent;//future parent for new node
+            while (true) {// to change what the node focus is
+                parent = focusNode;
+                if (parent.value.keyComp(value) == -1) {//using keyComp to compare values
+                    focusNode = focusNode.left;// if it is less than the root go to the left
+                    if (focusNode == null) {// if it has no child to the left
+                        parent.left = newNode;//set the left child to new node
+                        return;//to get us out of the infinite loop
+                    }
+                } else {
+                    focusNode = focusNode.right;// if we reach here we know that we need to put our node on the right child
+                    if (focusNode == null) {// if it is null add it to the right child
+                        parent.right = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void inOrderTrav(){
+        inOrderTraversal(root);
+    }
+    private void inOrderTraversal(TNode focusNode){
+        if(focusNode != null){
+            inOrderTraversal(focusNode.left);
+            System.out.println(focusNode);
+            inOrderTraversal(focusNode.right);
+        }
+    }
+
+    public void delete(Keyed value) {
+
+    }
+
+    public Keyed search(Keyed value) {
+        return null;
+    }
+
+    public void showTree() {
+
+    }
+
+    public int getAverageLevel(Keyed value) {
+        return -0;
+    }
+
+
+}
